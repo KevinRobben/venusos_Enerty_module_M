743d7f3a33eb94241c950df980fd1f1bc873fbaf
KevinRobben <kevinrobben1999@gmail.com>
2024-11-25 15:08:01 +0100

    refactor serial port initialization for improved clarity

--- module_m_decoder.py
+++ module_m_decoder.py
@@ -190,7 +190,11 @@ class ModuleM:
             port_name = f"/dev/{port_name}"
 
         try:
-            self.ser = serial.Serial(port_name, 115200, timeout=1)
+            self.ser = serial.Serial()
+            self.ser.port = port_name
+            self.ser.baudrate = 115200
+            self.ser.timeout = 1
+            self.ser.open()
         except Exception as e:
             logging.error('Could not connect to Module M: Exception when within init serial port: %s', e.args[0])
             self.ser = None
