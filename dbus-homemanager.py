73fa36795f91be743c0ffd2e5e29770852f5b067
KevinRobben <kevinrobben1999@gmail.com>
2024-12-30 18:48:13 +0100

    test

--- dbus-homemanager.py
+++ dbus-homemanager.py
@@ -43,7 +43,7 @@ class DbusENERTYService:
         self._dbusservice.add_path('/HardwareVersion', 0)
         self._dbusservice.add_path('/Connected', 1)
         self._dbusservice.add_path('/Serial', "00000000000")
-        self._dbusservice.add_path('/Error/0/Id', '0')
+        self._dbusservice.add_path('/ErrorCode', 0)
         self._dbusservice.add_path('/Ac/Power', 0, gettextcallback=self._get_text_for_w)
         self._dbusservice.add_path('/Ac/L1/Voltage', 0, gettextcallback=self._get_text_for_v)
         self._dbusservice.add_path('/Ac/L2/Voltage', 0, gettextcallback=self._get_text_for_v)
@@ -75,11 +75,11 @@ class DbusENERTYService:
         if time.time() - self.last_error_switch > 10:
             self.last_error_switch = time.time()
             if len(self.module_m.errors) == 0:
-                self._dbusservice['/Error/0/Id'] = "fischerpanda:e-2"
+                self._dbusservice['/ErrorCode'] = 0
             else:
                 if self.module_m.errors_show_index > len(self.module_m.errors) - 1:
                     self.module_m.errors_show_index = 0
-                self._dbusservice['/Error/0/Id'] = self.module_m.errors[self.module_m.errors_show_index]
+                self._dbusservice['/ErrorCode'] = self.module_m.errors[self.module_m.errors_show_index]
                 self.module_m.errors_show_index += 1
 
         if self.module_m._read_data() and self.module_m._decode_data():
